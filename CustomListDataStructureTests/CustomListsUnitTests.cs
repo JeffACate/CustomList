c3a2b7fd821813c45af96f54d588030a815844c6
unknown <jeff.a.cate@gmail.com>
2020-02-24 10:34:27 -0600

    init

---
+++ CustomListDataStructureTests/CustomListsUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CustomListDataStructureTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListDataStructureTests
+{
+    [TestClass]
+    public class CustomeListUnitTests
+    {
+        [TestMethod]
+        public void Add_()
+        {
+        }
+    }
+}
---
+++ CustomListDataStructureTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListDataStructureTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListDataStructureTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af9ff83f-9230-4ecb-944b-47171afe43bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
